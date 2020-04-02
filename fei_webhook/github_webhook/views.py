3c6bfd8e9c3dfd021132520f828e9bdbe91496bc
pf <colingong@hotmail.com>
2020-04-03 02:24:51 +0800

    dummy

--- fei_webhook/github_webhook/views.py
+++ fei_webhook/github_webhook/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from share.util_file import log_event
+import json
 
 # Create your views here.
 def alive(request):
@@ -10,11 +11,10 @@ def alive(request):
 @csrf_exempt
 def webhook(request):
     if request.method == 'POST':
-        for k, _ in request.POST.items():
-            print(f'key ---> {k}')
-        print(request.POST.get('ref'))
-        print(request.POST.get('before'))
-        print(request.POST.get('after'))
+        data = json.loads(request.body)
+        print(data.get('ref'))
+        print(data.get('before'))
+        print(data.get('after'))
         log_event(request.body.decode('utf-8'))
         return HttpResponse('ok')
 
