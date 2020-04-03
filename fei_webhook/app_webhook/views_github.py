95900d6ce068a74d26816f7c5bc06a4b6a6d2568
pf <colingong@hotmail.com>
2020-04-03 11:13:42 +0800

    fix hmac sha1 check

--- fei_webhook/app_webhook/views_github.py
+++ fei_webhook/app_webhook/views_github.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from share.util_file import log_event
 import json
-import hashlib, hmac
+import hashlib, hmac, base64
 
 # Create your views here.
 @csrf_exempt
@@ -19,8 +19,12 @@ def webhook(request):
         # payload = request.body.decode('utf-8')
         # signature = hashlib.sha1(request.body).hexdigest()
         # sign = hmac.new('123456', request.body, hashlib.sha1)
-        sign = hmac.new('123456'.encode('utf-8'), request.body, hashlib.sha1)
+        raw = request.body
+        key = '123456'.encode('utf-8')
+        hashed = hmac.new(key, raw, hashlib.sha1)
+        sign = base64.encodebytes(hashed.digest()).decode('utf-8')
         print(f'check sign: {sign}')
+
         log_event(request.body.decode('utf-8'))
 
         # TODO: 如果是form
