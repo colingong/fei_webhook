06b82ad2ec7f0c2900087992231d58b093df6902
pf <colingong@hotmail.com>
2020-04-09 01:23:25 +0800

    csrf_exempt @ fei views

--- fei_webhook/app_webhook/views_github_fei.py
+++ fei_webhook/app_webhook/views_github_fei.py
@@ -2,7 +2,9 @@
    A Django demo
 """
 from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def fei_project(request):
     print(request.body)
     return HttpResponse('success')
